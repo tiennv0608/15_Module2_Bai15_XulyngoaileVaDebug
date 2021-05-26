0c013784f095de88ad4f12c5422f121359c8fb39
tiennv0608 <tiennv0608@gmail.com>
2021-05-26 20:48:48 +0700

    b1

---
+++ 00_Demo/src/TestException.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TestException {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Numerator:");
+        int numerator = scanner.nextInt();
+        System.out.printf("Denominator:");
+        int denominator = scanner.nextInt();
+        int result = numerator/denominator;
+        System.out.printf("\nResult: %d / %d = %d \n",numerator, denominator, result);
+    }
+}
