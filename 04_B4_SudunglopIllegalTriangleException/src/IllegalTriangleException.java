ba62d9fa6d9ed65b45c50072cbbda53a610237a4
tiennv0608 <tiennv0608@gmail.com>
2021-05-27 14:01:36 +0700

    b4

--- 04_B4_SudunglopIllegalTriangleException/src/IllegalTriangleException.java
+++ 04_B4_SudunglopIllegalTriangleException/src/IllegalTriangleException.java
@@ -7,8 +7,7 @@ public class IllegalTriangleException extends Exception {
     }
 
     public void check(Integer side1, Integer side2, Integer side3) throws IllegalTriangleException {
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0) throw new IllegalTriangleException();
-        if (!(side1 + side2 > side3) || !(side2 + side3 > side1) ||
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || !(side1 + side2 > side3) || !(side2 + side3 > side1) ||
                 !(side1 + side3 > side2)) throw new IllegalTriangleException();
     }
 }
